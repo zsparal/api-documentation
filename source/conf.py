7df88f867d34114f0ba608025485b0f333fe0c39
Robbert Schreuder Hes <robbert@mollie.com>
2018-05-07 22:15:59 +0200

    Reintroduce Pygments as a syntax highlighting fallback

--- source/conf.py
+++ source/conf.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import pygments
 
 # The parent directory has to be included for 'sphinx-autobuild' to work.
 sys.path.insert(0, os.path.abspath("../"))
@@ -56,6 +57,9 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
@@ -82,3 +86,18 @@ html_favicon = '_static/img/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add custom CSS overrides of the theme.
+def setup(app):
+    # enable Pygments json lexer
+    try:
+        if pygments.__version__ >= '1.5':
+            # use JSON lexer included in recent versions of Pygments
+            from pygments.lexers import JsonLexer
+        else:
+            # use JSON lexer from pygments-json if installed
+            from pygson.json_lexer import JSONLexer as JsonLexer
+    except ImportError:
+        pass  # not fatal if we have old (or no) Pygments and no pygments-json
+    else:
+        app.add_lexer('json', JsonLexer())
