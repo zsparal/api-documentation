7580ad734f8574c9deafb37a91e62d4806829f13
Maikel van der Panne <maikel@mollie.com>
2018-05-14 10:48:23 +0200

    Automatically add deprecation notice for V1 API endpoints.
    This fixes #76. It adds a generic deprecation notice for V1 API endpoints.
    
    TODO:
    - Currently, warnings have been added manually already. Because I added this deprecation notice, it now shows twice.
    - I haven't removed the manual deprecation notice yet, as we may want to adjust the automatic one to be endpoint specific, and contain links to the V2 API documentation, just like the manual notice has right now.

--- source/extensions/mollie/api_name_directive.py
+++ source/extensions/mollie/api_name_directive.py
@@ -1,8 +1,6 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 
-from source.extensions import utilities
-
 
 class ApiNameDirective(Directive):
     has_content = False
@@ -23,5 +21,22 @@ class ApiNameDirective(Directive):
             api_version_line["classes"].append("api-name__version")
 
             api_name_line += [api_version_line]
+
+            if self.options["version"] == 1:
+                warning_node = nodes.warning()
+
+                warning_text = nodes.line_block()
+                warning_par_1 = nodes.paragraph(text=
+                    "The v1 API has been deprecated. New features will go into the "
+                    "v2 API. We're recommending all new implementations to be built "
+                    "on top of the v2 API.")
+                warning_par_2 = nodes.paragraph(text=
+                    "The v1 API will be supported for the foreseeable future, at "
+                    "least until July 2023.")
+                warning_text += [warning_par_1, warning_par_2]
+
+                warning_node += warning_text
+
+                return [api_name_line, warning_node]
 
         return [api_name_line]
