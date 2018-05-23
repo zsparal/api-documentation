3a6d03eb798f991807c92fd9cefbc3f768bf3618
Vernon de Goede <vernon@mollie.com>
2018-05-23 11:41:39 +0200

    Check whether mobile nav is open while resizing to desktop viewport.

--- source/theme/js/mobileNavigationButton.js
+++ source/theme/js/mobileNavigationButton.js
@@ -1,4 +1,8 @@
+import { debounce } from "lodash";
 import { enhance } from "./utils";
+
+const NO_SCROLL_UTILITY_CLASS = "u-no-scroll";
+const MOBILE_NAV_BREAKPOINT = 980; // _variables.scss:19
 
 export default enhance("mobile-navigation-button", () => {
   const navigationTrigger = document.querySelector(".js-toggle-mobile-nav");
@@ -10,4 +14,24 @@ export default enhance("mobile-navigation-button", () => {
     navigationTrigger.classList.toggle("is-opened");
     document.documentElement.classList.toggle("u-no-scroll");
   });
+
+  const allowScrollingOnDesktop = () => {
+    const { documentElement } = document;
+    const documentScrollBlocked = documentElement.classList.contains(NO_SCROLL_UTILITY_CLASS);
+
+    if (documentScrollBlocked) {
+      requestAnimationFrame(() => {
+        const documentWidth = window.innerWidth;
+
+        // If we're on desktop and the mobile nav was previously opened, close it.
+        if (documentWidth > MOBILE_NAV_BREAKPOINT) {
+          documentElement.classList.remove(NO_SCROLL_UTILITY_CLASS);
+          mobileSidebar.classList.remove("is-visible");
+          navigationTrigger.classList.remove("is-opened");
+        }
+      });
+    }
+  };
+
+  window.addEventListener("resize", debounce(() => allowScrollingOnDesktop(), 200, false));
 });
