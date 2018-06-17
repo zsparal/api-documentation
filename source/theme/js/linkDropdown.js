eab1bff16c366a2bc514cd96fe2b02181f042f5a
Vernon de Goede <vernon@mollie.com>
2018-06-17 16:43:32 +0200

    Run Prettier for all JS files in source/theme/**/*.js

--- source/theme/js/linkDropdown.js
+++ source/theme/js/linkDropdown.js
@@ -1,12 +1,12 @@
-import { enhance } from "./utils";
-import Dropkick from "dropkickjs";
+import { enhance } from './utils';
+import Dropkick from 'dropkickjs';
 
-export default enhance("link-dropdown", element => {
-  element.addEventListener("change", () => {
+export default enhance('link-dropdown', element => {
+  element.addEventListener('change', () => {
     window.location = element.value;
   });
 
   new Dropkick(element, {
-    mobile: false
+    mobile: false,
   });
 });
--- source/theme/js/subNavigation.js
+++ source/theme/js/subNavigation.js
@@ -1,37 +1,37 @@
-import { enhance } from "./utils";
+import { enhance } from './utils';
 
 // Test via a getter in the options object to see if the passive property is accessed
 const detectPassiveScrollSupport = () => {
   let supportsPassive = false;
   try {
-    let opts = Object.defineProperty({}, "passive", {
+    let opts = Object.defineProperty({}, 'passive', {
       get: function() {
         supportsPassive = true;
       },
     });
-    window.addEventListener("testPassive", null, opts);
-    window.removeEventListener("testPassive", null, opts);
+    window.addEventListener('testPassive', null, opts);
+    window.removeEventListener('testPassive', null, opts);
   } catch (e) {}
 
   return supportsPassive;
 };
 
-export default enhance("sub-navigation", () => {
-  const subNavigation = document.querySelector(".js-sub-navigation");
-  const subNavigationItems = document.querySelectorAll(".js-sub-navigation-item");
+export default enhance('sub-navigation', () => {
+  const subNavigation = document.querySelector('.js-sub-navigation');
+  const subNavigationItems = document.querySelectorAll('.js-sub-navigation-item');
   const supportsPassive = detectPassiveScrollSupport();
 
   window.addEventListener(
-    "scroll",
+    'scroll',
     () => {
       requestAnimationFrame(() => {
         const offsetY = window.pageYOffset;
         if (offsetY > 120) {
-          subNavigationItems.forEach(node => node.classList.add("sub-navigation-item--small"));
-          subNavigation.classList.add("sub-navigation--small");
+          subNavigationItems.forEach(node => node.classList.add('sub-navigation-item--small'));
+          subNavigation.classList.add('sub-navigation--small');
         } else {
-          subNavigation.classList.remove("sub-navigation--small");
-          subNavigationItems.forEach(node => node.classList.remove("sub-navigation-item--small"));
+          subNavigation.classList.remove('sub-navigation--small');
+          subNavigationItems.forEach(node => node.classList.remove('sub-navigation-item--small'));
         }
       });
     },
