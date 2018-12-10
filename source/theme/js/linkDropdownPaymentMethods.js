465263621910851d48c380da19cad1d2fa717898
Albert Carreras <albert.carreras@mollie.com>
2018-12-10 17:08:46 +0100

    Adds comments to code and styles it

--- source/theme/js/linkDropdownPaymentMethods.js
+++ source/theme/js/linkDropdownPaymentMethods.js
@@ -3,6 +3,7 @@ import Dropkick from 'dropkickjs';
 
 const DEFAULT_METHOD = 'bancontact';
 
+// Adds the onchange listener that hides/shows methods based on selection
 export const linkDropdownPaymentMethods = enhance('payment-method-switcher', element => {
   element.addEventListener('change', () => {
     element.childNodes.forEach(method => {
@@ -17,12 +18,18 @@ export const linkDropdownPaymentMethods = enhance('payment-method-switcher', ele
       }
     });
   });
-
+  // Initializes the select element we created
   new Dropkick(element, {
     mobile: false,
   });
 });
+
+// Adds the payment method dropdown to the DOM
+// We need to do this with JS, as the doc file is compiled from the .rst files
 export const addPaymentMethodDropdown = () => {
+  //Gets all possible payment methods and the preferred one from storage
+
+  // We need to get the payment methods block (different names in different pages, same prefix)
   const paymentMethodsBlock = document.querySelectorAll('[id^=payment-method]');
   const storedMethod = localStorage.getItem('preferredPaymentMethod');
   let preferredMethod;
@@ -34,20 +41,32 @@ export const addPaymentMethodDropdown = () => {
   if (paymentMethodsBlock.length) {
     const paymentMethods = paymentMethodsBlock[0].childNodes;
     let options = '';
+
+    // Adds the selector option for each method
     paymentMethods.forEach(method => {
+      // Inside the block, we are interested in the divs, not the title nor the description
       if (method.nodeName === 'DIV') {
+        // Defined which one is selected && hide the rest
         const isSelected = method.id === preferredMethod;
         if (!isSelected) {
           method.classList.add('u-screenreader-only');
           method.setAttribute('aria-hidden', 'true');
         }
+        // Build the options, with their names and if they are selected or not
         const methodName = method.childNodes[1].textContent.replace('¶', '');
         options += `<option value="${method.id}" ${
           isSelected ? ' selected' : ''
         }>${methodName}</option>`;
       }
     });
-    const dropdown = `<select data-enhancer="payment-method-switcher" class="link-dropdown">${options}</select>`;
-    paymentMethods[1].insertAdjacentHTML('afterbegin', dropdown);
+    // Creates select element
+    const dropdown = document.createElement('select');
+    // Adds attributes
+    dropdown.setAttribute('data-enhancer', 'payment-method-switcher');
+    dropdown.classList.add('link-dropdown', 'payment-methods');
+    // Adds the options we generated before
+    dropdown.innerHTML = options;
+    // Adds it after the title and description (there are text nodes, that's why the index is 5)
+    paymentMethodsBlock[0].insertBefore(dropdown, paymentMethods[5]);
   }
 };
